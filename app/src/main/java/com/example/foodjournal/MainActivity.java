0b8e7bf8e941639fd105de30bf343aca6933bf27
Alfred Ortega <jusalfort07@byui.edu>
2021-02-17 18:16:57 +0800

    Second Comment by Justine Ortega

--- app/src/main/java/com/example/foodjournal/MainActivity.java
+++ app/src/main/java/com/example/foodjournal/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // First comment test
+        // Second comment by Justine Ortega
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
