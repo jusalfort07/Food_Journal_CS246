50934a39a5b2ec78a210ae7126fde421398ddc17
Michael <sie20003@byui.edu>
2021-02-24 21:07:11 +1100

    local test

---
+++ app/src/test/java/com/example/foodjournal/UserSettingsTest.java
@@ -0,0 +1,31 @@
+package com.example.foodjournal;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UserSettingsTest {
+    @Test
+    public void verify_UserSettings() {
+        UserSettings user1 = new UserSettings("Michael", "mike@email.com", true, "weekly");
+
+        assertEquals("Michael", user1.name);
+        assertEquals("mike@email.com", user1.email);
+        assertEquals(true, user1.sendReport);
+        assertEquals("weekly", user1.frequency);
+    }
+}
+
+class UserSettings {
+    String name;
+    String email;
+    Boolean sendReport;
+    String frequency;
+
+    public UserSettings(String name, String email, Boolean sendR, String freq) {
+        this.name = name;
+        this.email = email;
+        this.sendReport = sendR;
+        this.frequency = freq;
+    }
+}
