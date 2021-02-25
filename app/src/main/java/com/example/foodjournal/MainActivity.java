163fed9c918c27603c7eb0dbd0e982573b6fe015
Michael <sie20003@byui.edu>
2021-02-25 22:29:45 +1100

    main screen-menu, new resources and theme.

--- app/src/main/java/com/example/foodjournal/MainActivity.java
+++ app/src/main/java/com/example/foodjournal/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.foodjournal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnStart, btnView, btnSettings;
+    View appBackGround;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,14 @@ public class MainActivity extends AppCompatActivity {
         // Third comment from Michael for testing...can someone please confirm that they can see this.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set main background
+        appBackGround = findViewById(R.id.appbg);
+        appBackGround.setBackgroundResource(R.drawable.bg_blue);
+
+        // Assigning buttons
+        btnStart = findViewById(R.id.btn_start);
+        btnView = findViewById(R.id.btn_view);
+        btnSettings = findViewById(R.id.btn_settings);
     }
 }
