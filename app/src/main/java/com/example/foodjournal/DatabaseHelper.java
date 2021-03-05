1d228f022dca497a22ba360919e63c4d290b3a64
Michael <sie20003@byui.edu>
2021-03-05 18:21:29 +1100

    Created database connection codes with insert command to allow saving of record from Food Entry view.

---
+++ app/src/main/java/com/example/foodjournal/DatabaseHelper.java
@@ -0,0 +1,78 @@
+package com.example.foodjournal;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "FoodJournal_CS246.db";
+    public static final String TABLE_NAME = "food_entry";
+    //public static final String COL1 = "ID";
+    public static final String COL2 = "FOOD_TYPE";
+    public static final String COL3 = "FOOD_DESCRIPTION";
+    public static final String COL4 = "FOOD_QUANTITY";
+    public static final String COL5 = "DATE_INTAKE";
+    public static final String COL6 = "FOOD_COMMENTS";
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + TABLE_NAME +
+                " (ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "  FOOD_TYPE TEXT, FOOD_DESCRIPTION TEXT, FOOD_QUANTITY INTEGER, " +
+                "  DATE_INTAKE TEXT, FOOD_COMMENTS TEXT)";
+        db.execSQL(createTable);
+    }
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String upgradeTable = "DROP TABLE IF EXISTS " + TABLE_NAME;
+        db.execSQL(upgradeTable);
+        onCreate(db);
+    }
+
+
+    public boolean insertData(String foodType, String foodDesc, int foodQty,
+                              String dateIntake, String foodComments) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL2, foodType);
+        contentValues.put(COL3, foodDesc);
+        contentValues.put(COL4, foodQty);
+        contentValues.put(COL5, dateIntake);
+        contentValues.put(COL6, foodComments);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+        return res;
+    }
+}
+
+
+
+
+
+
+
+
+
